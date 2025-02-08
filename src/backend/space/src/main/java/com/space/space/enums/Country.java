16c05f6ced3b49e84d3f2e3a8107ce8003aee48b
Vengala Revanth <pvengalarevanth22@gmail.com>
2025-02-08 18:34:21 -0500

    User Management

--- src/backend/space/src/main/java/com/space/space/enums/Country.java
+++ src/backend/space/src/main/java/com/space/space/enums/Country.java
@@ -3,4 +3,9 @@ package com.space.space.enums;
 public enum Country {
     USA,
     CANADA,
+    INDIA,
+    AUSTRALIA,
+    GERMANY,
+    NEPAL,
+    GHANA,
 }
--- src/backend/space/src/main/java/com/space/space/enums/Language.java
+++ src/backend/space/src/main/java/com/space/space/enums/Language.java
@@ -2,4 +2,8 @@ package com.space.space.enums;
 
 public enum Language {
     ENGLISH,
+    FRENCH,
+    HINDI,
+    TELUGU,
+    GERMAN
 }
